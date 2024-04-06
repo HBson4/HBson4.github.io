f45b39546a4d291464a7b5b301a3e7c1a7d5fd9c
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-04-05 20:25:02 -0600

    Swag boss

---
+++ scripts/base.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    adjustSectionHeight(); // Call on page load
+  
+    $(window).resize(function() {
+      adjustSectionHeight(); // Call on window resize
+    });
+  
+    function adjustSectionHeight() {
+      $('.parallax-section').each(function() {
+        var sectionHeight = $(this).height();
+        var contentHeight = $(this)[0].scrollHeight;
+        
+        // Check if content overflows
+        if (contentHeight > sectionHeight) {
+          $(this).css('min-height', contentHeight + 'px'); // Adjust min-height
+        } else {
+          $(this).css('min-height', '100vh'); // Reset to 100vh if no overflow
+        }
+      });
+    }
+  });
