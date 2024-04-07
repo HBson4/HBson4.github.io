7e16d23b71eafcde8e05441df281e37ff7bd1137
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-04-06 21:52:33 -0600

    swag boss

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,7 +1,11 @@
 const darkModeToggle = document.getElementById('dark-mode-checkbox');
 const main = document.querySelector('main');
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
 
 // Event listener for the dark mode toggle
 darkModeToggle.addEventListener('change', function () {
     main.classList.toggle('dark');
+    header.classList.toggle('dark');
+    footer.classList.toggle('dark');
 });
