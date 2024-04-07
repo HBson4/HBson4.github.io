a8a736f6dcf8411806235aff8f91915ec9244d9c
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-04-07 11:16:48 -0600

    new font sizes

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -2,10 +2,12 @@ const darkModeToggle = document.getElementById('dark-mode-checkbox');
 const main = document.querySelector('main');
 const header = document.querySelector('header');
 const footer = document.querySelector('footer');
+const html = document.querySelector('html');
 
 // Event listener for the dark mode toggle
 darkModeToggle.addEventListener('change', function () {
     main.classList.toggle('dark');
     header.classList.toggle('dark');
     footer.classList.toggle('dark');
+    html.classList.toggle('dark');
 });
